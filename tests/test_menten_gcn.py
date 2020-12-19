66fe2c400a8d8baf31193cadbccb73859b071402
Jack Maguire <jackmaguire1444@gmail.com>
2020-12-19 16:16:13 +0000

    dots

--- tests/test_menten_gcn.py
+++ tests/test_menten_gcn.py
@@ -13,6 +13,7 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.regularizers import l2
 
+import numpy as np
 
 def test_main():
     pass
@@ -100,11 +101,11 @@ def observe_NEE3(Xval, Aval, Eval, N, F, S):
 
 def test_NEE3():
     '''
-         2
-        / \
-       1 - 3
-        \ /
-         0
+         2       .
+        / \      .
+       1 - 3     .
+        \ /      .
+         0       .
     '''
 
     N = 5  # one is missing
