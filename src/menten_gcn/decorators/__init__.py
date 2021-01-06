5563a0da3b467721a98a63fc4e6f4f4c4b7731b4
Jack Maguire <jackmaguire1444@gmail.com>
2021-01-06 14:00:10 +0000

    noqa in __init__

--- src/menten_gcn/decorators/__init__.py
+++ src/menten_gcn/decorators/__init__.py
@@ -1,5 +1,5 @@
-from .base import *
-from .geometry import *
-from .rosetta import *
-from .sequence import *
-from .standard import *
+from .base import *      # noqa: F401,F403
+from .geometry import *  # noqa: F401,F403
+from .rosetta import *   # noqa: F401,F403
+from .sequence import *  # noqa: F401,F403
+from .standard import *  # noqa: F401,F403
