13a5549dbc6c5da466668f3977877e78447ea67f
Jack Maguire <jackmaguire1444@gmail.com>
2021-01-06 13:41:56 +0000

    noqa in __init__

--- src/menten_gcn/__init__.py
+++ src/menten_gcn/__init__.py
@@ -1,9 +1,9 @@
-from . import decorators
-from . import util
-from . import playground
+from . import decorators           # noqa: F401,F403
+from . import util                 # noqa: F401,F403
+from . import playground           # noqa: F401,F403
 
-from .data_maker import DataMaker
-from .data_management import *
-from .wrappers import *
+from .data_maker import DataMaker  # noqa: F401,F403
+from .data_management import *     # noqa: F401,F403
+from .wrappers import *            # noqa: F401,F403
 
 __version__ = '0.0.0'
