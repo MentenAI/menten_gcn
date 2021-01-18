ff7a5e9e8e1790cd1a7fe00bd473d4ca84a82dd4
Jack Maguire <jackmaguire1444@gmail.com>
2021-01-18 16:29:45 +0000

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='menten-gcn',
-    version='0.1.0',
+    version='0.2.0',
     license='MIT',
     description='This package decorates graph tensors with data from protein models',
     long_description='%s' % (
