94e6592a04baa3df2105be59408d3279d760115f
Jack Maguire <jackmaguire1444@gmail.com>
2021-02-02 09:30:30 -0500

    Update nbody_convs.py

--- src/menten_gcn/playground/nbody_convs.py
+++ src/menten_gcn/playground/nbody_convs.py
@@ -342,7 +342,7 @@ def flat2_unnamed_util(n, A_int, final_t, prefix):
                     name=prefix + "_flat2_unnamed_util_reshape")
     condition_indices = tf.dynamic_partition(r2, A_int, 2)
 
-    s_1 = tf.shape(condition_indices[0])[0]
+    s_1 = tf.shape(condition_indices[0])[0].value
     s_2 = int(final_t)
     s = [s_1, s_2]
     zero_padding1 = tf.zeros(shape=s)
