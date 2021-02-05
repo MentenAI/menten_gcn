c8ecc5ad1200b303fff601da8547bd1cc031aa51
Jack Maguire <jackmaguire1444@gmail.com>
2021-02-05 15:26:11 +0000

    ../../.bumpversion.cfg

--- src/menten_gcn/__init__.py
+++ src/menten_gcn/__init__.py
@@ -1,10 +1,11 @@
 from . import decorators           # noqa: F401,F403
 from . import util                 # noqa: F401,F403
 from . import playground           # noqa: F401,F403
-from . import published            # noqa: F401,F403
 
 from .data_maker import DataMaker  # noqa: F401,F403
 from .data_management import *     # noqa: F401,F403
 from .wrappers import *            # noqa: F401,F403
 
-__version__ = '0.0.0'
+from . import published            # noqa: F401,F403
+
+__version__ = '0.2.1'
